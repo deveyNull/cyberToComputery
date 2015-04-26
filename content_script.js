7336af2ef2d037b305f4708c1d02426d78077b74
Dennis Devey <m171458@usna.edu>
2015-04-25 23:26:33 -0400

    1st

---
+++ content_script.js
@@ -0,0 +1,41 @@
+walk(document.body);
+
+function walk(node) 
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+	
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
+
+	
+	v = v.replace(/Cyber/g, "Computery"); 
+	v = v.replace(/cyber/g, "computery"); 
+	
+	
+	
+	textNode.nodeValue = v;
+}
